ce3366c54b20a7e4370b9ff8887206753234a26e
Dmitriy <D.Zhebratkin.FIT.121.20@knute.edu.ua>
2021-05-15 16:40:04 +0300

    first commit

---
+++ recursia/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace recursia
+{
+    class Program
+    {  static void Example(string str)
+        {  
+            if(str.Length > 0)
+                Example(str.Substring(1, str.Length - 1));
+                else
+                    return;
+            Console.Write(str[0]);
+            
+        
+         }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введiть речення:");
+            string s = Console.ReadLine();
+            Console.WriteLine("Результат:");
+            Example(s);
+            Console.ReadLine();
+
+        }
+    }
+}
